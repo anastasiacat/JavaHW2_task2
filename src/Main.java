9ce2ac849c05ce79c1e0f596bb436256efb2b203
Anastasia I <niavolk@gmail.com>
2023-01-07 16:48:53 +0400

    Rename variable

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ public class Main {
         int balance = 100; //сумма на счету клиента
         int refill = 1100; //сумма пополнения счета
         int resultBalance; //итоговая сумма на счету клиента
-        int bonuce = 0; //бонусные рубли за пополнение счета
+        int bonus = 0; //бонусные рубли за пополнение счета
 
         if (refill > 1000) {
-            bonuce = refill / 100;
+            bonus = refill / 100;
         }
 
-        resultBalance = balance + refill + bonuce;
-
-        System.out.println("Клиент пополнил счет на " + refill + " рублей(ь/я) - бонус равен " + bonuce +
+        resultBalance = balance + refill + bonus;
+        System.out.println("Клиент пополнил счет на " + refill + " рублей(ь/я) - бонус равен " + bonus +
                 " рублям(ей), итоговая сумма на счету клиента - " + resultBalance + " рублей(ь/я)");
     }
 }
